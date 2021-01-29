bf4415d2446d1195ff9d0404e7812d328919649e
ariadnimac <ariadnimac>
2021-01-29 11:38:38 +0200

    add comments explaining the structure of a credit card number

--- src/lib/isCreditCard.js
+++ src/lib/isCreditCard.js
@@ -4,6 +4,13 @@ import assertString from './util/assertString';
 const creditCard = /^(?:4[0-9]{12}(?:[0-9]{3,6})?|5[1-5][0-9]{14}|(222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}|6(?:011|5[0-9][0-9])[0-9]{12,15}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11}|6[27][0-9]{14})$/;
 /* eslint-enable max-len */
 
+/* A credit catd number consists of 10-19 digits structured as follows:
+1. 6-8 digit Issue Identification Number (IIN)
+2. up to 12 digits account identifier 
+3. a single digit in the end calvulated with Luhn's algorithm
+*/
+
+
 export default function isCreditCard(str) {
   assertString(str);
   const sanitized = str.replace(/[- ]+/g, '');
